9c3efbf7e590b4adc8ec9fa78c15d67ae954f032
KIRAN <sahukirankumar548@gmail.com>
2026-05-06 16:55:30 +0530

    Update contract address after redeployment to Sepolia

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 import contractABI from "./SecureVote.json";
 
-export const contractAddress = "0x1b39D0620500BBa9bB8aB0fe090720f55Ea792ec";
+export const contractAddress = "0x587c8548A7D94639E358f992f8E3506Aac3BF045";
 export const abi = contractABI.abi;
